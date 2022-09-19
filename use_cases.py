531ad01353ff179d56ac638a28f5acf3fcd13e48
Eric Bruhns <bruhe002@gmail.com>
2022-09-18 18:26:44 -0700

    Skeleton code for use case functions

---
+++ use_cases.py
@@ -0,0 +1,42 @@
+
+# Use cases for Calculator
+
+
+# Button functions
+def addPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def subPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def multPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def divPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def numPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+def clearPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def decimalPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def equalsPress(opPhase: bool, secondInputPhase: bool):
+    pass
+
+# Logic Functions
+def changeOperation(opPhase: bool, secondInputPhase: bool):
+    pass
+
+
+def automaticOperation(opPhase: bool, secondInputPhase: bool):
+    pass
