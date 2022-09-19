1993108594f7b628dad4c1476391779d90ee7f39
Eric Bruhns <Eric.Bruhns@jpl.nasa.gov>
2022-09-19 12:25:33 -0700

    Edited buttons

--- calculator_gui.py
+++ calculator_gui.py
@@ -4,7 +4,7 @@ rootWindow = Tk()
 
 
 # Create margins for window
-rootWindow.geometry('332x520')
+# rootWindow.geometry('332x520')
 rootWindow.resizable(False, False)
 
 # row 0
@@ -26,7 +26,7 @@ eightBtn.grid(row=1,column=1)
 nineBtn = Button(rootWindow, text='9', font='20', padx=31, pady=30)
 nineBtn.grid(row=1,column=2)
 
-addBtn = Button(rootWindow, text='+', font='20', padx=31, pady=30, bg='#D3D3D3')
+addBtn = Button(rootWindow, text='+', font='20', padx=30, pady=30, bg='#D3D3D3')
 addBtn.grid(row=1,column=3)
 
 # row 2
@@ -39,7 +39,7 @@ fiveBtn.grid(row=2,column=1)
 sixBtn = Button(rootWindow, text='6', font='20', padx=31, pady=30)
 sixBtn.grid(row=2,column=2)
 
-subBtn = Button(rootWindow, text='-', font='30', padx=33, pady=30, bg='#D3D3D3')
+subBtn = Button(rootWindow, text='-', font='31', padx=33, pady=30, bg='#D3D3D3')
 subBtn.grid(row=2,column=3)
 
 # row 3
@@ -56,13 +56,13 @@ multBtn = Button(rootWindow, text='X', font='20', padx=30, pady=30, bg='#D3D3D3'
 multBtn.grid(row=3,column=3)
 
 # row 4
-decimalBtn = Button(rootWindow, text='.00', font='19', padx=24, pady=30, bg='#D3D3D3')
+decimalBtn = Button(rootWindow, text='.00', font='19', padx=22, pady=30, bg='#D3D3D3')
 decimalBtn.grid(row=4,column=0)
 
 zeroBtn = Button(rootWindow, text='0', font='20', padx=31, pady=30)
 zeroBtn.grid(row=4,column=1)
 
-equalBtn = Button(rootWindow, text='=', font='20', padx=31, pady=30, bg='#D3D3D3')
+equalBtn = Button(rootWindow, text='=', font='20', padx=30, pady=30, bg='#D3D3D3')
 equalBtn.grid(row=4,column=2)
 
 divBtn = Button(rootWindow, text='/', font='21', padx=33, pady=30, bg='#D3D3D3')
