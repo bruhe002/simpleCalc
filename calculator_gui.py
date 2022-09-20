99833a569382de6f08057f399c9f8fec923f2182
Eric Bruhns <bruhe002@gmail.com>
2022-09-20 15:28:18 -0700

    Ran several tests

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from use_cases import *
 
-opDict = {'operand': '', 'operator': ''}
+opDict = {'operand': '', 'operator': '', 'equalsFlag': False, 'secondInputFirstDigit': False}
+
 
 rootWindow = Tk()
 
@@ -16,43 +17,43 @@ entry.grid(row=0, column=1, columnspan=3)
 entry.insert(0, "0")
 entry.config(state= "readonly")
 
-clearBtn = Button(rootWindow, text='C', font='18', bg='#CC3E3E', padx=29, pady=30, command=lambda: clearPress(entry))
+clearBtn = Button(rootWindow, text='C', font='18', bg='#CC3E3E', padx=29, pady=30, command=lambda: clearPress(entry, opDict))
 clearBtn.grid(row=0, column=0)
 
 # row 1
-sevenBtn = Button(rootWindow, text='7', font='20', padx=31, pady=30, command=lambda: numPress(7, entry))
+sevenBtn = Button(rootWindow, text='7', font='20', padx=31, pady=30, command=lambda: numPress(7, entry, opDict))
 sevenBtn.grid(row=1,column=0)
 
-eightBtn = Button(rootWindow, text='8', font='20', padx=31, pady=30, command=lambda: numPress(8, entry))
+eightBtn = Button(rootWindow, text='8', font='20', padx=31, pady=30, command=lambda: numPress(8, entry, opDict))
 eightBtn.grid(row=1,column=1)
 
-nineBtn = Button(rootWindow, text='9', font='20', padx=31, pady=30, command=lambda: numPress(9, entry))
+nineBtn = Button(rootWindow, text='9', font='20', padx=31, pady=30, command=lambda: numPress(9, entry, opDict))
 nineBtn.grid(row=1,column=2)
 
 addBtn = Button(rootWindow, text='+', font='20', padx=30, pady=30, bg='#D3D3D3', command=lambda: addPress(opDict, entry))
 addBtn.grid(row=1,column=3)
 
 # row 2
-fourBtn = Button(rootWindow, text='4', font='20', padx=31, pady=30, command=lambda: numPress(4, entry))
+fourBtn = Button(rootWindow, text='4', font='20', padx=31, pady=30, command=lambda: numPress(4, entry, opDict))
 fourBtn.grid(row=2,column=0)
 
-fiveBtn = Button(rootWindow, text='5', font='20', padx=31, pady=30, command=lambda: numPress(5, entry))
+fiveBtn = Button(rootWindow, text='5', font='20', padx=31, pady=30, command=lambda: numPress(5, entry, opDict))
 fiveBtn.grid(row=2,column=1)
 
-sixBtn = Button(rootWindow, text='6', font='20', padx=31, pady=30, command=lambda: numPress(6, entry))
+sixBtn = Button(rootWindow, text='6', font='20', padx=31, pady=30, command=lambda: numPress(6, entry, opDict))
 sixBtn.grid(row=2,column=2)
 
 subBtn = Button(rootWindow, text='-', font='31', padx=33, pady=30, bg='#D3D3D3', command=lambda: subPress(opDict, entry))
 subBtn.grid(row=2,column=3)
 
 # row 3
-oneBtn = Button(rootWindow, text='1', font='20', padx=31, pady=30, command=lambda: numPress(1, entry))
+oneBtn = Button(rootWindow, text='1', font='20', padx=31, pady=30, command=lambda: numPress(1, entry, opDict))
 oneBtn.grid(row=3,column=0)
 
-twoBtn = Button(rootWindow, text='2', font='20', padx=31, pady=30, command=lambda: numPress(2, entry))
+twoBtn = Button(rootWindow, text='2', font='20', padx=31, pady=30, command=lambda: numPress(2, entry, opDict))
 twoBtn.grid(row=3,column=1)
 
-threeBtn = Button(rootWindow, text='3', font='20', padx=31, pady=30, command=lambda: numPress(3, entry))
+threeBtn = Button(rootWindow, text='3', font='20', padx=31, pady=30, command=lambda: numPress(3, entry, opDict))
 threeBtn.grid(row=3,column=2)
 
 multBtn = Button(rootWindow, text='X', font='20', padx=30, pady=30, bg='#D3D3D3', command=lambda: multPress(opDict, entry))
@@ -62,7 +63,7 @@ multBtn.grid(row=3,column=3)
 decimalBtn = Button(rootWindow, text='.00', font='19', padx=24, pady=30, bg='#D3D3D3', command=lambda: decimalPress(entry))
 decimalBtn.grid(row=4,column=0)
 
-zeroBtn = Button(rootWindow, text='0', font='20', padx=31, pady=30, command=lambda: numPress(0, entry))
+zeroBtn = Button(rootWindow, text='0', font='20', padx=31, pady=30, command=lambda: numPress(0, entry, opDict))
 zeroBtn.grid(row=4,column=1)
 
 equalBtn = Button(rootWindow, text='=', font='20', padx=30, pady=30, bg='#D3D3D3', command=lambda: equalsPress(opDict, entry))
--- use_cases.py
+++ use_cases.py
@@ -3,18 +3,37 @@ from typing import Dict
 # Use cases for Calculator
 
 
+
 # Button functions
-def clearPress(input: Entry):
+def clearPress(input: Entry, myDict: Dict):
+    myDict['operator'] = ''
+    myDict['operand'] = ''
+    myDict['equalsFlag'] = False
+    myDict['secondInputFirstDigit'] = False
+
     input.config(state='normal')
     input.delete(0, END)
     input.insert(0, '0')
     input.config(state='readonly')
 
 
-def numPress(btnVal: int, input: Entry):
+def numPress(btnVal: int, input: Entry, myDict: dict):
     input.config(state='normal')
+    # if operator was pressed, clear the screen on next input
     if input.get() == '0':
         input.delete(0, END)
+
+    # Deletes current input if operator is pressed
+    if not myDict['operator'] == '' and myDict['secondInputFirstDigit']:
+        input.delete(0, END)
+        myDict['secondInputFirstDigit'] = False
+
+    #If equals was recently pressed and the second input's first digit is being pressed
+    if myDict['equalsFlag'] :
+        input.delete(0, END)
+        myDict['equalsFlag'] = False
+        
+
     input.insert(END, str(btnVal))
     input.config(state='readonly')
     
@@ -23,21 +42,29 @@ def numPress(btnVal: int, input: Entry):
 def addPress(myDict: dict, input: Entry):
     myDict['operand'] = input.get()
     myDict['operator'] = '+'
+    myDict['equalsFlag'] = False
+    myDict['secondInputFirstDigit'] = True
 
 
 def subPress(myDict: dict, input: Entry):
     myDict['operand'] = input.get()
     myDict['operator'] = '-'
+    myDict['equalsFlag'] = False
+    myDict['secondInputFirstDigit'] = True
 
 
 def multPress(myDict: dict, input: Entry):
     myDict['operand'] = input.get()
     myDict['operator'] = 'X'
+    myDict['equalsFlag'] = False
+    myDict['secondInputFirstDigit'] = True
 
 
 def divPress(myDict: dict, input: Entry):
     myDict['operand'] = input.get()
     myDict['operator'] = '/'
+    myDict['equalsFlag'] = False
+    myDict['secondInputFirstDigit'] = True
 
 def decimalPress(input: Entry):
     input.config(state='normal')
@@ -54,24 +81,30 @@ def decimalPress(input: Entry):
 def equalsPress(myDict: dict, input: Entry):
     input.config(state='normal')
     operator = myDict['operator']
-    operandOne = float(myDict['operand']) if '.' in myDict['operand'] else int(myDict['operand'])
-    operandTwo = float(input.get()) if '.' in input.get() else int(input.get())
+    if operator != '':
+        operandOne = float(myDict['operand']) if '.' in myDict['operand'] else int(myDict['operand'])
+        operandTwo = float(input.get()) if '.' in input.get() else int(input.get())
 
-    value = 0
-
-    if operator == '+':
-        value = operandOne + operandTwo    
-    elif operator == '-':
-        value = operandOne - operandTwo
-    elif operator == 'X':
-        value = operandOne * operandTwo
-    elif operator == '/':
-        value = int(operandOne / operandTwo) if operandOne % operandTwo == 0 else float(operandOne / operandTwo)
     
-    myDict['operand'] = str(value)
 
-    input.delete(0, END)
-    input.insert(0, str(value))
+    
+        value = 0
+        if operator == '+':
+            value = operandOne + operandTwo    
+        elif operator == '-':
+            value = operandOne - operandTwo
+        elif operator == 'X':
+            value = operandOne * operandTwo
+        elif operator == '/':
+            value = int(operandOne / operandTwo) if operandOne % operandTwo == 0 else float(operandOne / operandTwo)
+
+        myDict['operand'] = str(value)
+        # myDict['operator'] = ''
+        myDict['equalsFlag'] = True
+        myDict['secondInputFirstDigit'] = False
+
+        input.delete(0, END)
+        input.insert(0, str(value))
 
     input.config(state='readonly')
 
